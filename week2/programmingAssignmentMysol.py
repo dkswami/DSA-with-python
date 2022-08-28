4e62b33d9a48ba325a89173e40e3f891ceba4620
dkswami <dkswami1820@gmail.com>
2022-08-28 14:43:36 +0530

    week 2 matched program added

--- week2/programmingAssignmentMysol.py
+++ week2/programmingAssignmentMysol.py
@@ -4,3 +4,22 @@ def mystringreverse(m):
         reversedstring = reversedstring[:] + m[i] 
     return(reversedstring)
 
+def intreverse(n):
+  ans = 0
+  while n > 0:
+    (d,n) = (n%10,n//10)
+    ans = 10*ans + d
+  return(ans)
+
+def matched(s):
+    nested = 0
+    for i in range(0, len(s)):
+        if s[i] == '(':
+            nested += 1
+        elif s[i] == ')':
+            nested -= 1
+            if nested < 0:
+                return(False)
+    return(nested == 0)
+
+print(matched(')sdw%('))
