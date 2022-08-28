707da5c41de69ea848a86ab8d3b99aad151a7831
dkswami <dkswami1820@gmail.com>
2022-08-28 14:59:50 +0530

    week 2 programming assignment completed

--- week2/programmingAssignmentMysol.py
+++ week2/programmingAssignmentMysol.py
@@ -22,4 +22,28 @@ def matched(s):
                 return(False)
     return(nested == 0)
 
-print(matched(')sdw%('))
+#print(matched(')sdw%('))
+
+def factors(n):
+    flist = []
+    for i in range(1, n+1):
+        if n%i==0:
+            flist.append(i)
+    return(flist)
+#print(factors(56))
+
+def isPrime(n):
+    return(factors(n) == [1,n])
+#print(isPrime(13))
+#print(isPrime(24))
+
+def sumprimes(l):
+    sum=0
+    for i in l:
+        if isPrime(i):
+            sum += i
+    return(sum)
+
+print(sumprimes([3,3,1,13]))
+print(sumprimes([2,4,6,9,11]))
+print(sumprimes([-3,1,6]))
